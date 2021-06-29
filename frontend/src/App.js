8f8d024c8231d52a97887dc0e42003cefc7c8d61
Saiyann <knt4xyz@gmail.com>
2021-06-29 16:02:11 +0200

    📦️ React Intalation ✏️ copy pasted index.html to the App,js 📝 Cretaed the data.js file to describe product list in array

---
+++ frontend/src/App.js
@@ -0,0 +1,146 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+            <header className="rowFlexJustify">
+                <div>
+                    <a className="brand" href="index.html">Amazone SaiYann</a>
+                </div>
+                <div>
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sing In</a>
+                </div>
+            </header>
+            <main>
+                <div className="rowFlexJustify center">
+                    <div className="card">
+                        <a href="productl.html">
+                            
+                            <img className="medium" src="./assets/p1.jpg" alt="product"/>
+                        </a>
+                        <div className="card__Body rating">
+                            <a href="prdocut.html">
+                                <h2>Nike slim Shirt</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star-halfo"></i></span>
+                                <span><i className="fa fa-star-o"></i></span>
+                            </div>
+                            <div className="price">€120</div>
+                        </div>
+                        
+                    </div>
+                    <div className="card">
+                        <a href="productl.html">
+                            
+                            <img className="medium" src="./assets/p1.jpg" alt="product"/>
+                        </a>
+                        <div className="card__Body rating">
+                            <a href="prdocut.html">
+                                <h2>Nike slim Shirt</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                            </div>
+                            <div className="price">€120</div>
+                        </div>
+                        
+                    </div>
+                    <div className="card">
+                        <a href="productl.html">
+                            
+                            <img className="medium" src="./assets/p1.jpg" alt="product"/>
+                        </a>
+                        <div className="card__Body rating">
+                            <a href="prdocut.html">
+                                <h2>Nike slim Shirt</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                            </div>
+                            <div className="price">€120</div>
+                        </div>
+                        
+                    </div>
+                    <div className="card">
+                        <a href="productl.html">
+                            
+                            <img className="medium" src="./assets/p1.jpg" alt="product"/>
+                        </a>
+                        <div className="card__Body rating">
+                            <a href="prdocut.html">
+                                <h2>Nike slim Shirt</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                            </div>
+                            <div className="price">€120</div>
+                        </div>
+                        
+                    </div>
+                    <div className="card">
+                        <a href="productl.html">
+                            
+                            <img className="medium" src="./assets/p1.jpg" alt="product"/>
+                        </a>
+                        <div className="card__Body rating">
+                            <a href="prdocut.html">
+                                <h2>Nike slim Shirt</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                            </div>
+                            <div className="price">€120</div>
+                        </div>
+                        
+                    </div>
+                    <div className="card">
+                        <a href="productl.html">
+                            
+                            <img className="medium" src="./assets/p1.jpg" alt="product"/>
+                        </a>
+                        <div className="card__Body rating">
+                            <a href="prdocut.html">
+                                <h2>Nike slim Shirt</h2>
+                            </a>
+                            <div className="rating">
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                                <span><i className="fa fa-star"></i></span>
+                            </div>
+                            <div className="price">€120</div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </main>
+            <footer className="rowFlexJustify center">
+                <p>All right reserved</p>
+            </footer >
+        </div>
+  );
+}
+export default App;
