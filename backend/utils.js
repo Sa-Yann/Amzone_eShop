8f48196585b1b6eb18ce03c11dd91193b7943bfd
Saiyann <knt4xyz@gmail.com>
2021-08-12 14:29:51 +0200

    ✏️detailed the way the const isAuth is thought for better understanding

--- backend/utils.js
+++ backend/utils.js
@@ -14,18 +14,32 @@ export const genrateToken = (user) => {
     })
 }
 
-// Building a middleware to define the user that is ordering 
+// Building a middleware to authetificate the user that is ordering 
 export const isAuth = (req, res, next) => {
     const authorisation = req.headers.authorization;
     if(authorisation) {
-        const token = authorisation.slice(7, authorisation.length); //why Slice 7: Bearer thetoken623756026HBU9DC90CHNC0C => Bearer + space is 7 so Slice 7
+        const token = authorisation.slice(7, authorisation.length); 
+        //why Slice 7: Bearer thetoken623756026HBU9DC90CHNC0C => Bearer + space is 7 so Slice 7
+        // slice(7, authorisation.length) means fron the 8th caracter to the end which is authorisation.lenght
         jwt.verify(
+            // verify decrypt the Token
             token,
             process.env.JWT_SECRET || 'somethingsecret',
             (err, decode) => {
+                // decode contains the data inside the Token
+                
                 if (err) {
                     res.status(401).send({ message: 'Invailde Token' })
                 } else {
+                // when building generateToken we define all the needed user infos : 
+                // {
+                //     _id: user._id,
+                //     name: user.name,
+                //     email:user.email,
+                //     isAdmin: user.isAdmin
+                // }
+                // so via decode we can retrive all saved user info that were saved in the token 
+                // that s why can set those user datas in req.user 
                     req.user = decode;
                     next();
                 }
